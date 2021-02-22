2ff5d87b4bb67918725b36f0cf3c75d9aa628f73
Clidenor Issac <issaccabrall@gmail.com>
2021-02-22 19:15:00 -0300

    Guerreiro com Exceção

--- Projeto 03 Guerreiro/MataOrcs.java/src/mataorcs/java/MataOrcsJava.java
+++ Projeto 03 Guerreiro/MataOrcs.java/src/mataorcs/java/MataOrcsJava.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MataOrcsJava 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Guerreiro magaiva = new Guerreiro("Pepeu", 100, 5);
         Scanner scanner = new Scanner(System.in);
         
@@ -18,25 +18,35 @@ public class MataOrcsJava
         
         while(true)
         {
-            String line = scanner.nextLine();
-               
-            if(line.equals("end")){
-                break;
-            }else if(line.equals("atacar")){
-                magaiva.atacar();
-            }else if(line.equals("regenerar")){
-                magaiva.regenerar();
-            }else if(line.equals("upar")){
-                magaiva.uparG();
-            }else if(line.equals("show")){
-                System.out.println(magaiva);
-            }else if(line.equals("Show orc")){
-                System.out.println(magaiva.monstro);
-            }else{
-                System.out.println("Fail: entrada inválida!");
-            }
+            try{
+                String line = scanner.nextLine();
+                
+                String aux;
+                System.out.println("Deseja continua? s para sim. n pra não");
+                aux = scanner.next();
 
-            
+                if(aux != "s" || aux != "n"){
+                    throw new RuntimeException("fail: Erro de entrada!");
+                }
+                
+                if(line.equals("end")){
+                    break;
+                }else if(line.equals("atacar")){
+                    magaiva.atacar();
+                }else if(line.equals("regenerar")){
+                    magaiva.regenerar();
+                }else if(line.equals("upar")){
+                    magaiva.uparG();
+                }else if(line.equals("show")){
+                    System.out.println(magaiva);
+                }else if(line.equals("Show orc")){
+                    System.out.println(magaiva.monstro);
+                }else{
+                    System.out.println("Fail: entrada inválida!");
+                }
+            }catch(RuntimeException e){
+                e.getMessage();
+            }
         }
     }
 }
